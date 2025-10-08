c0cc1752f4366f3695d92a12364680b9abdc8e99
maspa5z <pablomassereyes935@gmail.com>
2025-10-08 09:13:57 +0200

    jsp

---
+++ Dog.php
@@ -0,0 +1,10 @@
+<?php
+
+class Dog extends Animal {
+    private int $age;
+    private string $color;
+
+    public function __construct(string $name = "Inconnu", int $age = 0, string $color = "Noir") {
+        parent:: __construct($name);
+    }
+}
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 include "Animal.php";
 //inclure la classe Cat
 include "Cat.php";
+//inclure la classe Dog
+include "Dog.php";
 //instancier un objet de la classe Cat
 //Sans constructeur
 $monPremierChat = new Cat();
@@ -11,3 +13,7 @@ $monPremierChat->setName("Julie");
 $monDeuxiemeChat = new Cat("Mia", 2, "Gris");
 
 echo "Mon premier chat est " . $monPremierChat->getName() . " et mon deuxième est " . $monDeuxiemeChat->getName();
+
+$monPremierChien = new Dog();
+$monPremierChien->setName("Moscou");
+echo "\nMon premier chien est " . $monPremierChien->getName();
